720cc07b8138f3a36c9ee2cfc42399d03e5d8ed9
dongsu.han <kku304@naver.com>
2024-06-15 17:02:47 +0900

    change bg from zustand

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Category from "./components/Category";
 
 const page = async () => {
-  return <div className="min-h-[600px]">HomePage</div>;
+  return (
+    <div className="min-h-[600px]">
+      <Category />
+    </div>
+  );
 };
 
 export default page;
